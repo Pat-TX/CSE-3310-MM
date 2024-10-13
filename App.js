cb75bd9e9a68a5363f18175a3b6128a9692eb181
Pat-TX <williams.patrick.d@gmail.com>
2024-10-13 06:11:06 -0500

    set up stack navigation in app.js

--- App.js
+++ App.js
@@ -1,9 +1,19 @@
 import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Welcome from "./app/screens/Welcome";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   console.log("App executed");
 
-  return <Welcome />;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Welcome">
+        <Stack.Screen name="Welcome" component={Welcome} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
