8480f2f12f98e499565ba4134d2fa601ab095663
Pat-TX <williams.patrick.d@gmail.com>
2024-10-13 00:54:31 -0500

    moved things around
    created assets, screens folder, welcome screen

---
+++ app/screens/Welcome.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StatusBar } from 'react-native-web';
+import { View, StyleSheet, Text } from 'react-native';
+
+function Welcome(props) {
+    return (
+        <View style={styles.container}>
+          <Text>Open up App.js to start working on your app!</Text>
+          <StatusBar style="auto" />
+        </View>
+      );
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
+
+export default Welcome;
