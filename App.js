476ab3a2ee367a697cf0d306ff25fcd17e605764
Pat-TX <williams.patrick.d@gmail.com>
2024-10-13 08:53:09 -0500

    created screens

--- App.js
+++ App.js
@@ -4,6 +4,10 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Welcome from "./app/screens/Welcome";
 import PreUserLogin from "./app/screens/PreUserLogin";
 import PreMechLogin from "./app/screens/PreMechLogin";
+import UserLogin from "./app/screens/UserLogin";
+import UserRegister from "./app/screens/UserRegister";
+import MechLogin from "./app/screens/MechLogin";
+import MechRegister from "./app/screens/MechRegister";
 
 const Stack = createNativeStackNavigator();
 
@@ -16,6 +20,10 @@ export default function App() {
         <Stack.Screen name="Welcome" component={Welcome} />
         <Stack.Screen name="PreUserLogin" component={PreUserLogin} />
         <Stack.Screen name="PreMechLogin" component={PreMechLogin} />
+        <Stack.Screen name="UserLogin" component={UserLogin} />
+        <Stack.Screen name="UserRegister" component={UserRegister} />
+        <Stack.Screen name="MechLogin" component={MechLogin} />
+        <Stack.Screen name="MechRegister" component={MechRegister} />
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ app/screens/MechLogin.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MechLogin(props) {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default MechLogin;
