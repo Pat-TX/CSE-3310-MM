e2c66848f7b8b23ed5d30f487c08c72d7332c000
Pat-TX <williams.patrick.d@gmail.com>
2024-10-13 08:24:37 -0500

    Create PreUserLogin.js
    this page precedes registration and login

---
+++ app/screens/PreUserLogin.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import { SafeAreaView, View, StyleSheet, Text, TouchableOpacity, Dimensions, Image } from 'react-native';
+
+function Welcome(props) {
+    return (
+        <SafeAreaView style={styles.container}>
+
+          <Image 
+            style={styles.logo}
+            source={require('../assets/icon.png')}
+          />
+
+          <Text style={styles.header1}>Auto services?</Text>
+          <Text style={styles.header2}>We've got you covered.</Text>
+
+          <TouchableOpacity style={styles.buttonStyle}>
+            <Text style={styles.buttonText}>Login</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.buttonStyle}>
+            <Text style={styles.buttonText}>Register</Text>
+          </TouchableOpacity>
+
+        </SafeAreaView>
+      );
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    header1: {
+      fontSize: 40,
+      paddingBottom: 15,  
+    },
+    header2: {
+      fontSize: 20,
+      paddingBottom: 30,  
+    },
+    buttonStyle: {
+      elevation: 8,
+      backgroundColor: "lightblue",
+      borderRadius: 15,
+      paddingVertical: 10,
+      width: Dimensions.get('window').width - 50,  
+      margin: 10,
+    },
+    buttonText: {
+      fontSize: 18,
+      fontWeight: "bold",
+      alignSelf: "center",
+    },
+    logo: {
+      alignItems: "center",
+      justifyContent: "center",
+      width: 150,
+      height: 150,
+      margin: 20,
+    },
+  });
+
+export default Welcome;
