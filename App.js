53356cb905f7f58cb5e8bd256dfc434e533a7945
Pat-TX <williams.patrick.d@gmail.com>
2024-10-13 08:26:49 -0500

    added preuserlogin to stack

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Welcome from "./app/screens/Welcome";
+import PreUserLogin from "./app/screens/PreUserLogin";
 
 const Stack = createNativeStackNavigator();
 
@@ -12,6 +13,7 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Welcome">
         <Stack.Screen name="Welcome" component={Welcome} />
+        <Stack.Screen name="PreUserLogin" component={PreUserLogin} />
       </Stack.Navigator>
     </NavigationContainer>
   );
