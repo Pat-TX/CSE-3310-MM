91276589aacd0058e53bd4359c9e2ab625dc159a
Pat-TX <williams.patrick.d@gmail.com>
2024-10-13 21:33:27 -0500

    skeleton for login/reg pages

--- app/screens/MechLogin.js
+++ app/screens/MechLogin.js
@@ -1,11 +1,56 @@
 import React from 'react';
+import { SafeAreaView, View, StyleSheet, Text, TouchableOpacity, Dimensions, Image } from 'react-native';
 
 function MechLogin(props) {
     return (
-        <div>
-            
-        </div>
-    );
+        <SafeAreaView style={styles.container}>
+
+          <Image 
+            style={styles.logo}
+            source={require('../assets/icon.png')}
+          />
+
+          <Text style={styles.header1}>Login</Text>
+
+          <TouchableOpacity style={styles.buttonStyle} onPress={() => navigation.navigate('UserLogin')}>
+            <Text style={styles.buttonText}>Login</Text>
+          </TouchableOpacity>
+
+        </SafeAreaView>
+      );
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    header1: {
+      fontSize: 40,
+      paddingBottom: 15,  
+    },
+    buttonStyle: {
+      elevation: 8,
+      backgroundColor: "lightblue",
+      borderRadius: 15,
+      paddingVertical: 10,
+      width: Dimensions.get('window').width - 50,  
+      margin: 10,
+    },
+    buttonText: {
+      fontSize: 18,
+      fontWeight: "bold",
+      alignSelf: "center",
+    },
+    logo: {
+      alignItems: "center",
+      justifyContent: "center",
+      width: 150,
+      height: 150,
+      margin: 20,
+    },
+  });
 
 export default MechLogin;
