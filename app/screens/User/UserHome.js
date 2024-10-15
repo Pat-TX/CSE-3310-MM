2819f0fad4837deb4b84c810ec60bd3b384e8376
Pat-TX <williams.patrick.d@gmail.com>
2024-10-15 12:11:58 -0500

    created home screens

---
+++ app/screens/User/UserHome.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { SafeAreaView, View, StyleSheet, Text, TouchableOpacity, Dimensions, Image } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { styles } from './style';
+
+function UserHome({navigation}) {
+    return (
+        <SafeAreaView style={styles.container}>
+
+          <Text style={styles.header1}>User Home</Text>
+
+        </SafeAreaView>
+      );
+}
+
+export default UserHome;
