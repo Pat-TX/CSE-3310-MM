a69faf883f4700aa415c3af774f5feb72d457f7e
Pat-TX <williams.patrick.d@gmail.com>
2024-10-15 12:15:32 -0500

    fixed import

--- app/screens/Mech/MechHome.js
+++ app/screens/Mech/MechHome.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { SafeAreaView, View, StyleSheet, Text, TouchableOpacity, Dimensions, Image } from 'react-native';
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { styles } from './style';
+import { styles } from '../style';
 
 function MechHome({navigation}) {
     return (
