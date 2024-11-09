c1e55616a43ccfc2eaeab885cbd5c1cbd43c6f97
Pat-TX <williams.patrick.d@gmail.com>
2024-11-08 19:54:05 -0600

    updated userhome

--- app/screens/User/UserHome.js
+++ app/screens/User/UserHome.js
@@ -1,14 +1,57 @@
 import React from 'react';
 import { SafeAreaView, View, StyleSheet, Text, TouchableOpacity, Dimensions, Image } from 'react-native';
+import { Button } from '@react-navigation/elements';
 import { styles } from '../style';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import { NavigationContainer, useNavigation, NavigationIndependentTree } from '@react-navigation/native';
+
+const Tab = createMaterialTopTabNavigator();
+
+function MyTabs() {
+  return (
+    <Tab.Navigator tabBarPosition='bottom'>
+      <Tab.Screen name="Search" component={Search} />
+      <Tab.Screen name="Messages" component={Messages} />
+      <Tab.Screen name="Profile" component={Profile} />
+    </Tab.Navigator>
+  );
+}
+
+function Search() {
+  const navigation = useNavigation();
+
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Search Screen!</Text>
+    </View>
+  );
+}
+
+function Messages() {
+  const navigation = useNavigation();
+
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Messages Screen!</Text>
+    </View>
+  );
+}
+
+function Profile() {
+  const navigation = useNavigation();
+
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Profile Screen!</Text>
+    </View>
+  );
+}
 
 function UserHome({navigation}) {
     return (
-        <SafeAreaView style={styles.container}>
-
-          <Text style={styles.header1}>User Home</Text>
-
-        </SafeAreaView>
+      
+      <MyTabs />
+      
       );
 }
 
