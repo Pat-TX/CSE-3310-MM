a27f410834e357ccfb379141097066253e688d39
Sujana Kabir <kabirsujana@gmail.com>
2024-12-01 19:30:54 -0600

    final

--- app/screens/User/payment.js
+++ app/screens/User/payment.js
@@ -1,6 +1,13 @@
 import React, { useState } from 'react';
-import { SafeAreaView, View, StyleSheet, Text, TouchableOpacity, Image } from 'react-native';
-import { styles } from '../style';
+import {
+  SafeAreaView,
+  View,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  Image,
+} from 'react-native';
+import { styles } from '../newstyle'; 
 
 function Payment({ navigation }) {
   const [paymentMethod, setPaymentMethod] = useState(null);
@@ -8,46 +15,43 @@ function Payment({ navigation }) {
   const selectPaymentMethod = (method) => {
     setPaymentMethod(method);
     alert(`Payment method selected: ${method}`);
-    
   };
 
   return (
     <SafeAreaView style={styles.container}>
-      <TouchableOpacity
-        style={styles.goBackButton}
-        onPress={() => navigation.goBack()}
-      >
-        <Text style={styles.goBackText}>Go Back</Text>
-      </TouchableOpacity>
-      <Image 
-        style={styles.paymentlogo}
-        source={require('../../assets/handpay.png')}
-      />
-
-      <Text style={styles.paymentHeader}>Please Select Your Payment Method</Text>
-
-      <View style={styles.paymentOptions}>
-        <TouchableOpacity 
-          style={[styles.paymentStyle, paymentMethod === 'cash' && styles.selectedButton]}
-          onPress={() => selectPaymentMethod('cash')}
+      <View style={styles.topSection}>
+        <TouchableOpacity
+          style={styles.goBackButton}
+          onPress={() => navigation.goBack()}
         >
-          <Text style={styles.paymentText}>Cash</Text>
+          <Text style={styles.goBackText}>Go Back</Text>
         </TouchableOpacity>
-
-        <TouchableOpacity 
-          style={[styles.paymentStyle, paymentMethod === 'credit' && styles.selectedButton]}
-          onPress={() => selectPaymentMethod('credit')}
-        >
-          <Text style={styles.paymentText}>Credit</Text>
-        </TouchableOpacity>
+        <Image 
+          style={styles.paymentlogo}
+          source={require('../../assets/handpay.png')}
+        />
       </View>
-      <Image 
-        style={styles.pay}
-        source={require('../../assets/pay.png')}
-      />
+
+      <View style={styles.box}>
+        <Text style={styles.paymentHeader}>Please Select Your Payment Method</Text>
+        <View style={styles.paymentOptions}>
+          <TouchableOpacity
+            style={[styles.paymentStyle, paymentMethod === 'cash' && styles.selectedButton]}
+            onPress={() => selectPaymentMethod('cash')}
+          >
+            <Text style={styles.paymentText}>Cash</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={[styles.paymentStyle, paymentMethod === 'credit' && styles.selectedButton]}
+            onPress={() => selectPaymentMethod('credit')}
+          >
+            <Text style={styles.paymentText}>Credit</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </SafeAreaView>
   );
 }
-
 
 export default Payment;
